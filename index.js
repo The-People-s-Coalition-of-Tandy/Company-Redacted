045aa717211e8df4fbf57fc3c7393dd656ca0b28
Juilie <humphreythegray@gmail.com>
2023-01-02 17:21:54 -0500

    add swiping for mobile

--- index.js
+++ index.js
@@ -129,3 +129,48 @@ for (let i = 0; i < works.length; i++) {
   );
   works[i].addEventListener("mouseleave", removeWorksBackground);
 }
+
+document.addEventListener("touchstart", handleTouchStart, false);
+document.addEventListener("touchmove", handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return (
+    evt.touches || // browser API
+    evt.originalEvent.touches
+  ); // jQuery
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    /*most significant*/
+    if (xDiff > 0) {
+      selectedIndex--;
+      rotateCarousel();
+    } else {
+      selectedIndex++;
+      rotateCarousel();
+    }
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;
+}
